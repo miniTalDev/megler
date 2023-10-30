8bfee955d5d883c642b23d928f21a43a5fa1bf5e
miniTalDev <nasir522004@gmail.com>
2023-10-30 13:18:12 -0700

    detected

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import undetected_chromedriver as uc
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
