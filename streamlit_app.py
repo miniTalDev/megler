3b5ce877574b5dad93dcbc00ac6a4a89721d3585
miniTalDev <nasir522004@gmail.com>
2023-10-30 16:18:12 -0700

    sleep

--- streamlit_app.py
+++ streamlit_app.py
@@ -146,12 +146,16 @@ def get_pdf_url(url):
                                 <img src="https://raw.githubusercontent.com/miniTalDev/megler/master/reading.gif" alt="reading.gif" style="width: 500px; height: 500px;">
                             </div>
                             """
-    options = Options()
+    options = webdriver.ChromeOptions()
+    ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+    options.add_argument('--user-agent=' + ua)
+    options.add_argument("--headless")
     options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    chrome_service = fs.Service(executable_path=ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
-    driver = get_driver()
-    # driver = uc.Chrome(headless=True,use_subprocess=False)
+    driver = webdriver.Chrome(options=options,service=chrome_service)
     
     try:
         driver.get(url)
